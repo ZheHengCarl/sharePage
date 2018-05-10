86d95a2e7762332455ae61f226c12b25cc6afa1c
Carlxu <carl.zheheng@gmail.com>
2018-05-10 12:03:15 +0800

    first commit

---
+++ static/js/size.js
@@ -0,0 +1,18 @@
+﻿var changeFont = function () {
+    var nWdWidth = window.innerWidth;
+    var nFontSize = nWdWidth / 750 * 200;
+    console.log(nFontSize);
+    document.getElementsByTagName("html")[0].style.fontSize = nFontSize + "px";
+}
+changeFont();
+var changeHeight = function () {
+    var oWrap = document.querySelector("#app");
+    var wdH = window.innerHeight;
+    if (wdH > oWrap.offsetHeight) {
+        oWrap.style.minHeight = wdH + "px";
+        oWrap.style.minHeight = "initial";
+    }
+}
+window.onresize = function () {
+    changeFont();
+}
